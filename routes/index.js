f4c64ac76cfdcb659286ce3f350a132ccbc54db5
Mehdi <mfredj6@gmail.com>
2022-09-28 18:01:52 +0200

    varaible

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const OWM_API_KEY = process.env.OWM_API_KEY;
 
 
 router.get('/movies', (req, res) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}&language=fr`)
     .then(response => response.json())
     .then(data => {
       res.json(data)
