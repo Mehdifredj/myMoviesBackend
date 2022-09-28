a5bdbe834b8fc2f4109db60470c5a8db68d0cedc
Mehdi <mfredj6@gmail.com>
2022-09-28 16:04:20 +0200

    varaible

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://my-movies-backend-jm31.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://my-movies-frontend-e8te.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
